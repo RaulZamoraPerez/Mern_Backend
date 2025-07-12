a3cebc87471b449c4703304059fbe93cb9805ff2
Raul Emmanuel Zamora Perez <raulzamoraperez440@gmail.com>
2025-07-11 20:11:29 -0600

    Delete dist directory

--- dist/controllers/TeamController.d.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from "express";
-export declare class TeamMemberController {
-    static getProjectTeam: (req: Request, res: Response) => Promise<void>;
-    static findMemberByEmail: (req: Request, res: Response) => Promise<void>;
-    static addMemberById: (req: Request, res: Response) => Promise<void>;
-    static removeMemberById: (req: Request, res: Response) => Promise<void>;
-}
